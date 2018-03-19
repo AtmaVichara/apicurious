17623e38cd96a48dd017079cb529cb4cdc44a5f2
Joe Jobes <jmrjobes@gmail.com>
2018-03-19 13:06:24 -0600

    add profile and applicaiton controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ class SessionsController < ApplicationController
 
   def create
     binding.pry
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
+    redirect_to profile_index_path
   end
 
 end
