c05dfe9d723e9bd8fcfdf31b7df157e470c8b5aa
Joe Jobes <jmrjobes@gmail.com>
2018-03-19 15:04:59 -0600

    Add image and nickname to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319170836) do
+ActiveRecord::Schema.define(version: 20180319210335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180319170836) do
     t.datetime "updated_at", null: false
     t.string "oauth_token"
     t.string "oauth_secret_token"
+    t.string "nickname"
+    t.string "image"
   end
 
 end
