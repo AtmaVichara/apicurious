a07ebb79a1af446ee397a3b80751a6ca2b6a7f3c
Joe Jobes <jmrjobes@gmail.com>
2018-03-19 15:12:51 -0600

    Delete pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
-    binding.pry
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                  = auth_info.uid
       new_user.username             = auth_info.info.name
