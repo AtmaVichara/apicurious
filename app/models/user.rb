5178d893dd2b1d44858c1ea1f48e05fcdc98ae72
Joe Jobes <jmrjobes@gmail.com>
2018-03-19 15:12:10 -0600

    Add nickname and image to from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
+    binding.pry
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                  = auth_info.uid
-      new_user.username             = auth_info.extra.name
+      new_user.username             = auth_info.info.name
       new_user.oauth_token          = auth_info.credentials.token
       new_user.oauth_secret_token   = auth_info.credentials.secret
+      new_user.nickname             = auth_info.info.nickname
+      new_user.image                = auth_info.info.image
     end
   end
 end
