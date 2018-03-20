1080133227fede83f8249b8e0918cb9b94352db4
Joe Jobes <jmrjobes@gmail.com>
2018-03-20 07:54:58 -0600

    Add repo model

---
+++ spec/factories/repos.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :repo do
+    name "MyString"
+    full_name "MyString"
+    owner "MyString"
+    private "MyString"
+    fork "MyString"
+    html_url "MyString"
+    size "MyString"
+    language "MyString"
+    forks "MyString"
+    open_issues "MyString"
+    watchers "MyString"
+    default_branch "MyString"
+  end
+end
