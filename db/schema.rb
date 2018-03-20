b0d0c9eabb6f888a522a89e0b162efa79554a0d6
Joe Jobes <jmrjobes@gmail.com>
2018-03-20 11:16:40 -0600

    Add more fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320135255) do
+ActiveRecord::Schema.define(version: 20180320160850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180320135255) do
     t.string "oauth_secret_token"
     t.string "nickname"
     t.string "image"
+    t.string "repos"
+    t.string "bio"
   end
 
 end
